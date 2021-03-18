2754bc7dcf6f10b787936e9d66550a4c41353d14
Rachel Aizenman <racheliamn@gmail.com>
2021-03-18 16:24:21 -0400

    updated json

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const app = express()
 const path = require('path')
 const api = require('./routes/api')
 const bodyParser = require('body-parser')
-const favicon = require('express-favicon')
-app.use(favicon(__dirname + '/public/favicon.png'));
+const favicon = require("serve-favicon");
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "..", 'dist')))
@@ -17,6 +17,10 @@ app.use('/', api)
 
 
 const port = 3000
+
+app.get('/favico.ico', (req, res) => {
+    res.sendStatus(404);
+});
 
 app.listen(port, function() {
     console.log(`Running on port ${port}`)
